e6859746b55c209e2b86082de6aeb36da6e6f287
Amir Rahemeto <amiroimage@gmail.com>
2025-11-08 14:54:34 +0300

    Fix deployment path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   base: '/amir-portfolio/', // 👈 must match your GitHub repo name
+  plugins: [react()],
 });
